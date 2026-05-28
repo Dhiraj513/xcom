afc7fba139c0cb266e8e6f60aeb5052b861cd9b4
Dhiraj513 <121927685+Dhiraj513@users.noreply.github.com>
2026-05-28 20:37:50 +0545

    1.4

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,11 @@ export default defineConfig({
     react(),
      tailwindcss(),
   ],
+  theme: {
+    extend: {
+      fontFamily: {
+        dmSans:["DM Sans", "sans-serif"],
+      },
+    },
+  },
 })
