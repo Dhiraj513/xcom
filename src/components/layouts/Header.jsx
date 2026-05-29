005c8c3ab4ac6a4eabe9f87b2ac1660115bf44e8
Dhiraj513 <121927685+Dhiraj513@users.noreply.github.com>
2026-05-29 12:47:07 +0545

    1.9

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const header = () => {
   return (
-    <div className='w-full flex justify-between bg-gray-200'>
+    <div className='w-full flex justify-between bg-gray-200 px-5'>
 <h1 className='text-x1'> XECOM </h1>
-<ul className='flex'>
+<ul className='flex space-x-3'>
     <li> Home</li>
     <li>Mens</li>
     <li>Women</li>
