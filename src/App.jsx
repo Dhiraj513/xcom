44357dff6b6e417643b35220ff7e4b5408e18cf9
Dhiraj513 <121927685+Dhiraj513@users.noreply.github.com>
2026-05-30 11:45:35 +0545

    2.6

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import { useState } from 'react'
-import Header from './components/layouts/Header'
-import Footer from './components/layouts/Footer'
+import { Route, Routes } from 'react-router-dom'
 function App() {
  
 
   return (
     <>
-  <Header/>
-<div className='min-h-80' flex flex-col items-center justify-center>
-  <h2 className='text-2xl font-bold'>Welcome to XCOM</h2>
-</div>
-  <Footer/>
+  <Routes>
+    <Route path='/'></Route>
+  </Routes>
      </>
   )
 }
